64daeefebcc7cd69235a2f449fc7da15f94f145e
AndrewMishchenko <AndrewMishchenko001@gmail.com>
2021-07-10 00:19:39 +0300

    делает второе задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsNew = ingredients.map(ingredient => {
+  const ingredientNewItem = document.createElement('li');
+  ingredientNewItem.textContent = ingredient;
+  return ingredientNewItem;
+});
+
+ingredientsList.append(...ingredientsNew);
